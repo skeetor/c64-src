5c9bf15bd8a3828464e4dc0f1e43698b10acd571
Gerhard Gruber <gerhard.gruber@gdata.de>
2019-01-30 12:10:23 +0100

    Improved cc65 environment script to be callable from any directory.

--- cc65_env.bat
+++ cc65_env.bat
@@ -1,6 +1,13 @@
 @echo off
 
-call cc65_env_user.bat
+set CC65_HOME=%~dp0
+
+if exist %CC65_HOME%cc65_env_user.bat (
+   call %CC65_HOME%cc65_env_user.bat
+) else (
+    echo cc65_env_user.bat not found in local or parent director!
+    exit /B 1
+)
 
 set PATH=%PATH%;%CC65_HOME%\bin
 
