c19102b1015383033158fcb96f21b0138dffe990
Gerhard Gruber <gerhard.gruber@gdata.de>
2021-12-16 15:23:17 +0100

    Build script updated to allow invidual building of C128/C64 or both.

--- C128/spreddi/build.cmd
+++ C128/spreddi/build.cmd
@@ -1,26 +1,67 @@
 @echo off
+setlocal EnableDelayedExpansion
 
 if not exist "obj" mkdir obj
 if not exist "bin" mkdir bin
 
+set C1541_PATH=E:\Programme\WinVICE\3.1-x64\
+set "BUILD_128="
+set "BUILD_64="
+set "BUILD_TARGET="
+set PROJECT_NAME=spreddi
+set DISK_NAME=%PROJECT_NAME%
+
+del /F /Q obj\*.* > NUL 2>&1
+del /F /Q bin\%PROJECT_NAME%*.* > NUL 2>&1
+
+set BUILD_TARGET=%1
+if "%1" == "" goto no_params
+goto main
+
+:no_params
+set BUILD_128=True
+set BUILD_64=True
+goto build_src
+
+:main 
+if "%BUILD_TARGET%" == "c128" (
+set BUILD_128=True
+set BUILD_64=False
+goto build_src
+)
+
+if "%BUILD_TARGET%" == "c64" (
+set BUILD_128=False
+set BUILD_64=True
+goto build_src
+)
+
+:build_src
+
+if "%BUILD_128%" EQU "True" (
 set SYSTEM_CONFIG="c128-asm.cfg"
 set SYSTEM_DEFINE=c128
 
-@echo on
-echo Building %PROJECT_NAME%.s C128
-ca65 -I ..\..\include -W1 -g -l obj\%PROJECT_NAME%-%SYSTEM_DEFINE%.lst -mm near -t c64 -D C128 -o obj\%PROJECT_NAME%-%SYSTEM_DEFINE%.o %PROJECT_NAME%.s
-echo Done. Running linker
-echo=
-ld65 -C %SYSTEM_CONFIG% -Ln obj\%PROJECT_NAME%-%SYSTEM_DEFINE%.vice -vm -m obj\%PROJECT_NAME%-%SYSTEM_DEFINE%.map --dbgfile obj\%PROJECT_NAME%-c128.dbg -o bin\%PROJECT_NAME%-%SYSTEM_DEFINE%.prg obj\%PROJECT_NAME%-%SYSTEM_DEFINE%.o
+echo Building %PROJECT_NAME%.s !SYSTEM_DEFINE!
+ca65 -I ..\..\include -W1 -g -l obj\%PROJECT_NAME%-!SYSTEM_DEFINE!.lst -mm near -t c64 -D C128 -o obj\%PROJECT_NAME%-!SYSTEM_DEFINE!.o %PROJECT_NAME%.s
+ld65 -C !SYSTEM_CONFIG! -Ln obj\%PROJECT_NAME%-!SYSTEM_DEFINE!.vice -vm -m obj\%PROJECT_NAME%-!SYSTEM_DEFINE!.map --dbgfile obj\%PROJECT_NAME%-!SYSTEM_DEFINE!.dbg -o bin\%PROJECT_NAME%-!SYSTEM_DEFINE!.prg obj\%PROJECT_NAME%-!SYSTEM_DEFINE!.o
+)
 
+if "%BUILD_64%" == "True" (
 set SYSTEM_CONFIG="c64-asm.cfg"
 set SYSTEM_DEFINE=c64
 
-echo Building %PROJECT_NAME%.s C64
-ca65 -I ..\..\include -W1 -g -l obj\%PROJECT_NAME%-%SYSTEM_DEFINE%.lst -mm near -t c64 -D C64 -o obj\%PROJECT_NAME%-%SYSTEM_DEFINE%.o %PROJECT_NAME%.s
-echo Done. Running linker
-echo=
-ld65 -C %SYSTEM_CONFIG% -Ln obj\%PROJECT_NAME%-%SYSTEM_DEFINE%.vice -vm -m obj\%PROJECT_NAME%-%SYSTEM_DEFINE%.map --dbgfile obj\%PROJECT_NAME%-c128.dbg -o bin\%PROJECT_NAME%-%SYSTEM_DEFINE%.prg obj\%PROJECT_NAME%-%SYSTEM_DEFINE%.o
+echo Building %PROJECT_NAME%.s !SYSTEM_DEFINE!
+ca65 -I ..\..\include -W1 -g -l obj\%PROJECT_NAME%-!SYSTEM_DEFINE!.lst -mm near -t c64 -D C64 -o obj\%PROJECT_NAME%-!SYSTEM_DEFINE!.o %PROJECT_NAME%.s
+ld65 -C !SYSTEM_CONFIG! -Ln obj\%PROJECT_NAME%-!SYSTEM_DEFINE!.vice -vm -m obj\%PROJECT_NAME%-!SYSTEM_DEFINE!.map --dbgfile obj\%PROJECT_NAME%-!SYSTEM_DEFINE!.dbg -o bin\%PROJECT_NAME%-!SYSTEM_DEFINE!.prg obj\%PROJECT_NAME%-!SYSTEM_DEFINE!.o
+)
 
 copy /Y obj\*.vice bin 2>NUL: >NUL:
-%C1541_PATH%c1541.exe -format "develop,00" d64 bin\%DISK_NAME%.d64 -write "bin\%PROJECT_NAME%-c64.prg" "%PROJECT_NAME%-c64" -write "bin\%PROJECT_NAME%-c128.prg" "%PROJECT_NAME%-c128" -write "bin\spritedata.seq" "spritedata,s"
+%C1541_PATH%c1541.exe -format "develop,00" d64 bin\%DISK_NAME%.d64
+if "%BUILD_128%" == "True" (
+%C1541_PATH%c1541.exe -attach bin\%DISK_NAME%.d64 -write "bin\%PROJECT_NAME%-c128.prg" "%PROJECT_NAME%-c128"
+)
+if "%BUILD_64%" == "True" (
+%C1541_PATH%c1541.exe -attach bin\%DISK_NAME%.d64 -write "bin\%PROJECT_NAME%-c64.prg" "%PROJECT_NAME%-c64"
+)
+%C1541_PATH%c1541.exe -attach bin\%DISK_NAME%.d64 -write "bin\spritedata.seq" "spritedata,s"
